316a89de1edca1ea3de4772e86d34770b41f0d1e
funige <funigedev@gmail.com>
2020-09-21 15:09:28 +0900

    fixtree.phpを追加

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ futabizeは、[POTI-board改二](https://github.com/sakots/poti-kaini)を色々�
 ## [サンプル](http://futabize.websozai.jp)
 
 ## 設置方法
-config.phpと同じフォルダにfutabizeのフォルダをコピーした後、config.phpを編集します。
+config.phpと同じフォルダに[futabizeのフォルダ](https://github.com/funige/futabize/tree/master/futabize)をコピーした後、config.phpを編集します。
 
     define('SKIN_DIR', 'futabize/');
 
@@ -18,10 +18,29 @@ config.phpと同じフォルダにfutabizeのフォルダをコピーした後�
 - 動画記録はサポートしません
 - DynamicPaletteも無しの方向で
 
+## fixtree.phpについて
+fixtree.phpは、お絵かき掲示板 Poti-board用の外部phpプログラムです。
+
+スパムが投稿されて何年も前の古いコメントツリーが浮上してしまった場合などに、スレッドを「自然な順序」に並べ直すことができます。
+
+futabizeでは
+
+    [管理用] -> 記事削除 -> [tree.logを修復する]
+
+で、fixtree.phpを実行できます。  
+(fixtree.php は potiboard.php 等と同じ階層に置いてください)  
+
+まあ、ちゃんと設定してスパムの投稿をブロックすれば、こんなもの要らないはずなんですけど……
+
+
+## メモ
 
 Skinnyの外部テンプレートを使用しています。外部テンプレートのキャッシュはなかなか更新されないので、いつまでたっても古いのが出てくるときはcacheフォルダを丸ごと削除するといいです。
 
 ## 履歴
+
+#### ver1.0.1 (2020/09/22)
+- fixtree.phpを追加
 
 #### ver1.0.0 (2020/08/02)
 - 作成
---
+++ fixtree.php
@@ -0,0 +1,91 @@
+<?php
+//POTI-board plugin fixtree(c)2020 funige
+//v0.0.0
+
+include(__DIR__.'/config.php');
+
+login() or die("パスワードが違います");
+$line = file(LOGFILE);
+$tree = file(TREEFILE);
+save($tree, TREEFILE.".".time()) or die("バックアップが作成できません");
+
+foreach ($line as $i => $value) {
+    list($no,,,$email) = explode(",", rtrim($value));
+    $sage[$no] = (stripos($email,'sage') !== false);
+}
+
+foreach ($tree as $i => $value) {
+    $treelog[$i]["value"] = $value;
+    $treelog[$i]["key"] = lastPost($value);
+}
+
+usort($treelog, "cmp");
+save(array_column($treelog, "value"), TREEFILE) or die("上書き保存できません");
+
+foreach ($treelog as $item) {
+    echo($item["value"]);
+    echo("<br>");
+}
+
+echo("<hr>tree.logを修復しました。");
+echo("<a href='#' onclick='history.back();'>ログを更新してください。</a>");
+exit;
+
+
+//各スレッドのキー（最新の投稿No）を取得
+function lastPost($value) {
+    global $sage;
+    $arr = array_reverse(array_slice(explode(",", rtrim($value)), 0, MAX_RES));
+    foreach ($arr as $no) {
+        if (!$sage[$no]) {
+            return $no;
+        }
+    }
+    return end($arr);
+}
+
+//ソート
+function cmp($a, $b) {
+    if ($a["key"] == $b["key"]) {
+        return 0;
+    }
+    return ($a["key"] < $b["key"]) ? 1 : -1;
+}
+
+//保存
+function save($tree, $filename) {
+    $fp = fopen($filename, "wb");
+    if ($fp) {
+        flock($fp, LOCK_EX);
+        foreach ($tree as $line) {
+            fwrite($fp, $line);
+        }
+        fflush($fp);
+        flock($fp, LOCK_UN);
+        fclose($fp);
+        return true;
+    }
+    return false;
+}
+
+//パスワード入力画面
+function login() {
+    if (!$_POST) {
+        echo("<form action='fixtree.php' method='POST'>");
+        echo("<input type='password' name='pass'> ");
+        echo("<input type='submit'></form>");
+        exit;
+    }
+
+    if ($ADMIN_PASS) {
+        global $ADMIN_PASS;
+        $password = $ADMIN_PASS; //改二
+    } else {
+        $password = ADMIN_PASS; //旧バージョン
+    }
+    
+    if ($_POST["pass"] != $password) {
+        return false;
+    }    
+    return true;
+}
